1c4d93a5e4d552474bcb8e46ca2d667fd8cbdff4
Vasilios Eleftheriadis <vasilios.eleftheriadis@computacenter.com>
2022-01-26 12:01:45 +0100

    New Account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "aws.aft.ccvelefthe+aft-account-1@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "vasilios.eleftheriadis@computacenter.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
